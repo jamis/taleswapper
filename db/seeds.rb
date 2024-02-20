c7bfe9b1e6c972f56193ec99bf6c3a189fad353a
Jamis Buck <jamis@jamisbuck.org>
2024-02-20 06:53:16 -0700

    `dirs` is an array

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 dirs = [ File.expand_path('./seeds/fixtures', __dir__) ]
-sets = Dir[File.join(dirs, '*.yml')].map { |f| File.basename(f, '.yml').to_sym }
+sets = Dir[File.join(dirs.first, '*.yml')].map { |f| File.basename(f, '.yml').to_sym }
 
 ActiveRecord::FixtureSet.create_fixtures(dirs, sets)
