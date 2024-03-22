ddcf2405ce930352aa4b0d5a44a9da55cc84573b
Jamis Buck <jamis@jamisbuck.org>
2024-03-22 06:16:34 -0600

    also need to pin drafts module

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,14 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+
 pin "services"
 pin "utilities"
 pin "quill-config"
+pin "drafts"
+
 pin_all_from "app/javascript/quill-config", under: "quill-config"
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
