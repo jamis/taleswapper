4d8aaa04d9c1d1f38df3dbe7f0852470c907b353
Jamis Buck <jamis@jamisbuck.org>
2024-03-22 16:18:42 -0600

    only all-caps the first line of the first PRIMARY section

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,8 +1,11 @@
 module ChaptersHelper
   include ActiveSupport::NumberHelper
 
-  def render_section(section, first: false)
-    render partial: "sections/roles/#{section.role}", locals: { section: section, first: first }
+  def render_section(section)
+    first_primary_section = !@seen_first_primary_section && section.role == 'primary'
+    render(partial: "sections/roles/#{section.role}", locals: { section: section, first: first_primary_section }).tap do
+      @seen_first_primary_section ||= first_primary_section
+    end
   end
 
   def visible_sequels(chapter)
