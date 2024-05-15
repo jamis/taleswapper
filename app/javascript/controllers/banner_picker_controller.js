6e17cc3c869c5bdde3598bf6b8598871cdf6df8a
Jamis Buck <jamis@jamisbuck.org>
2024-05-14 20:22:24 -0600

    progress bar for banner image upload

--- app/javascript/controllers/banner_picker_controller.js
+++ app/javascript/controllers/banner_picker_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import ImagePicker from "../image_picker";
 
 export default class extends Controller {
-  static targets = [ 'blobField' ];
+  static targets = [ 'button', 'blobField', 'progressBar' ];
 
   static values = {
     directUploadUrl: String,
@@ -25,15 +25,25 @@ export default class extends Controller {
   }
 
   imageUploadAvailable(data) {
+    this.progressBarTarget.classList.replace('block', 'hidden');
+    this.buttonTarget.innerHTML = this.savedLabel;
+    this.buttonTarget.disabled = false;
+    this.buttonTarget.classList.remove('cursor-not-allowed');
+
     this.blobFieldTarget.value = data.signed_id;
     this.element.requestSubmit();
   }
 
   imageUploadWillBegin() {
-    // set up a progress bar or something, if desired
+    this.savedLabel = this.buttonTarget.innerHTML;
+    this.buttonTarget.innerHTML = "Uploading...";
+    this.buttonTarget.disabled = true;
+    this.buttonTarget.classList.add('cursor-not-allowed');
+    this.progressBarTarget.classList.replace('hidden', 'block');
+    this.progressBarTarget.style.width = "0%";
   }
 
   imageUploadDidProgress(pct) {
-    // update the progress bar, if desired
+    this.progressBarTarget.style.width = pct;
   }
 }
